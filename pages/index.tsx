5cfa5908b0a5594915a4bcb8519122335799458c
MiloMeng1688 <milomeng1688@gmail.com>
2025-04-01 10:27:01 +0800

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: 40 }}>
+      <h1 style={{ fontSize: 30 }}>🎯 我的 OKR 系统</h1>
+      <p>欢迎来到你的个人 OKR 工具页面！</p>
+      <ul>
+        <li>👉 设置目标（Objective）</li>
+        <li>👉 填写关键结果（Key Results）</li>
+        <li>👉 实时查看进度</li>
+      </ul>
+    </div>
+  )
+}
